c29e6715b0392af8477beb016838e75601ebb519
Oli Laban <olilaban@outlook.com>
2024-10-16 21:24:15 +0100

    Build

--- dist/alpine-tailwind-lightbox.esm.js
+++ dist/alpine-tailwind-lightbox.esm.js
@@ -1,4 +1,4 @@
-var x=`<div
+var g=`<div
     class="fixed inset-0 bg-black/90 touch-pinch-zoom z-[9999]"
     style="display: none;"
     role="region"
@@ -15,17 +15,26 @@ var x=`<div
     @keydown.right="$store.lightbox.next(group)"
 >
     <ul aria-live="polite" role="presentation" id="lightbox-list">
-        <template x-for="(item, index) in $store.lightbox.items[group]">
+        <template x-for="(item, index) in $store.lightbox.items[group]" :key="item.id">
             <li
                 class="absolute inset-0 flex justify-center items-center"
                 role="group"
                 aria-roledescription="slide"
-                x-show="$store.lightbox.show[group]?.el === item.el"
+                x-show="$store.lightbox.show[group]?.id === item.id"
                 x-transition.opacity.duration.300ms
                 :aria-label="\`\${index + 1} of \${$store.lightbox.items[group].length}\`"
                 @click.self="$store.lightbox.show[group] = null"
             >
-                <template x-if="item.type === 'image'">
+                <template x-if="item.type === 'image' && !item.loaded">
+                    <div>
+                        <svg aria-hidden="true" class="w-9 h-9 text-gray-600 animate-spin fill-white" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                        </svg>
+                        <span class="sr-only">Loading...</span>
+                    </div>
+                </template>
+                <template x-if="item.type === 'image' && item.loaded">
                     <img class="w-auto h-auto max-h-screen max-w-screen" :src="item.url" :alt="item.alt || ''">
                 </template>
                 <template x-if="item.type === 'embed'">
@@ -122,4 +131,4 @@ var x=`<div
         </svg>
     </a>
 </div>
-`;var p=null;function w(e){e.store("lightbox",{show:{},items:{},touchStart:null,onTouchStart(t){if(!t.changedTouches){this.touchStart=null;return}this.touchStart=t.changedTouches[0]},onTouchEnd(t,o){if(!t.changedTouches||!this.touchStart)return;let{screenX:i,screenY:a}=this.touchStart,{screenX:h,screenY:u}=t.changedTouches[0];if(!i||!a||!h||!u)return;let n=i-h,c=a-u;Math.abs(n)<Math.abs(c)||(n>=100?this.next(o):n<=-100&&this.prev(o))},prev(t){let o=this.items[t].findIndex(i=>i.el===this.show[t].el);this.show[t]=o===0?this.items[t][this.items[t].length-1]:this.items[t][o-1]},next(t){let o=this.items[t].findIndex(i=>i.el===this.show[t].el);this.show[t]=o===this.items[t].length-1?this.items[t][0]:this.items[t][o+1]}}),e.directive("lightbox",(t,{value:o,modifiers:i,expression:a},{effect:h,evaluateLater:u})=>{if(o==="group")return;if(!a){console.warn("Alpine warn: no url or config expression passed to x-lightbox",t);return}let n=u(a);h(()=>{n(c=>{let s=b(t,c);if(e.store("lightbox").show[s]===void 0&&(e.store("lightbox").show[s]=null),e.store("lightbox").items[s]??=[],!document.querySelector(`#lightbox-${s}`)){let r=document.createElement("template");r.innerHTML=x;let l=r.content.children[0];l.id=`lightbox-${s}`,l.setAttribute("x-data",`{ group: '${s}' }`),document.body.appendChild(l),setTimeout(()=>{l.hasOwnProperty("_x_isShown")||e.initTree(l)},15)}let m=e.store("lightbox").items,d=e.store("lightbox").items[s]?.findIndex(r=>r.el===t),g=v(c,s,t);d!==-1&&d!==void 0?m[s][d]=g:m[s].push(g),t.addEventListener("click",r=>{r.preventDefault(),e.store("lightbox").show[s]=e.store("lightbox").items[s].find(l=>l.el===t)})})})})}var b=(e,t)=>e.hasAttribute("x-lightbox:group")?e.getAttribute("x-lightbox:group"):t.group?String(t.group):(p??=(Math.random()+1).toString(36).substring(7),p),v=(e,t,o)=>(typeof e=="string"&&(e={url:e}),{el:o,type:"image",autoplay:!1,muted:!1,...e,group:t});var k=w;export{k as default};
+`;var p=null,v=[];function f(l){l.store("lightbox",{show:{},items:{},touchStart:null,onTouchStart(e){if(!e.changedTouches){this.touchStart=null;return}this.touchStart=e.changedTouches[0]},onTouchEnd(e,t){if(!e.changedTouches||!this.touchStart)return;let{screenX:i,screenY:s}=this.touchStart,{screenX:a,screenY:n}=e.changedTouches[0];if(!i||!s||!a||!n)return;let r=i-a,o=s-n;Math.abs(r)<Math.abs(o)||(r>=100?this.next(t):r<=-100&&this.prev(t))},loadImage(e,t){let i=this.items[t].findIndex(({id:a})=>a===e.id);[e,this.nextItem(i,t),this.prevItem(i,t)].filter(Boolean).forEach(({id:a,url:n,loaded:r})=>{if(r)return;let o=new Image;o.onload=()=>{let h=this.items[t].findIndex(d=>d.id===a);this.items[t][h].loaded=!0,v.push(o)},o.src=n})},open(e,t){let i=t?this.items[e].find(s=>s[typeof t=="object"?"el":"url"]===t):this.items[e][0]||null;i&&(this.show[e]=i,this.loadImage(i,e))},prevItem(e,t){return e===0?this.items[t][this.items[t].length-1]:this.items[t][e-1]},nextItem(e,t){return e===this.items[t].length-1?this.items[t][0]:this.items[t][e+1]},navigate(e,t){let i=this.items[t].findIndex(a=>a.id===this.show[t].id),s=e==="prev"?this.prevItem(i,t):this.nextItem(i,t);this.show[t]=s,this.loadImage(s,t)},prev(e){this.navigate("prev",e)},next(e){this.navigate("next",e)}});function c(e,t,i=null,s=null,a=[]){if(Array.isArray(e)||(e=[e]),!document.querySelector(`#lightbox-${t}`)){let r=document.createElement("template");r.innerHTML=g;let o=r.content.children[0];o.id=`lightbox-${t}`,o.setAttribute("x-data",`{ group: '${t}' }`),document.body.appendChild(o),setTimeout(()=>{o.hasOwnProperty("_x_isShown")||l.initTree(o)},15)}l.store("lightbox").show[t]??=null,l.store("lightbox").items[t]??=[];let n=l.store("lightbox").items;e.forEach(r=>{let o=l.store("lightbox").items[t]?.findIndex(d=>d.id===i),h=y(r,t,i,s,a);o!==-1&&o!==void 0?n[t][o]={...n[t][o],...h}:(h.loaded=!h.lazy,n[t].push(h))})}l.magic("lightbox",()=>{function e(t,i=null){t=t.map(s=>(typeof s=="string"&&(s={url:s}),s.id=u(),s)),c(t,i||m())}return e.open=(t=null,i=null)=>{l.store("lightbox").open(i||m(),t)},e}),l.directive("lightbox",(e,{value:t,modifiers:i,expression:s},{effect:a,evaluateLater:n})=>{if(t==="group")return;if(!s){console.warn("Alpine warn: no url or config expression passed to x-lightbox",e);return}let r=n(s),o=u(),h=!1;a(()=>{r(d=>{l.nextTick(()=>{let x=C(e,d);c(d,x,o,e,i),h||(e.addEventListener("click",w=>{w.preventDefault(),l.store("lightbox").open(x,e)}),h=!0)})})})})}var u=()=>(Math.random()+1).toString(36).substring(2,15),C=(l,c)=>l.hasAttribute("x-lightbox:group")?l.getAttribute("x-lightbox:group"):c.group?String(c.group):m(),m=()=>(p??=u(),p),y=(l,c,e=null,t=null,i=[])=>(typeof l=="string"&&(l={url:l}),{type:"image",lazy:i.includes("lazy"),autoplay:!1,muted:!1,id:e,...l,el:t,group:c});var T=f;export{T as default};
